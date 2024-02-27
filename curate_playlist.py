07e2e2f81e3259476ce2e7aab483ada0421903e0
Michael Goh <mikegoh1997@gmail.com>
2024-02-27 10:42:26 -0800

    Update curate_playlist.py
    remove descriptions from existing playlists

--- curate_playlist.py
+++ curate_playlist.py
@@ -49,7 +49,3 @@ playlists = [{'genre': 'TODO', 'title': 'TODO'}]
 
 for playlist in playlists:
 	create_playlist(playlist['genre'], playlist['title'])
-
-# DESCRIPTIONS
-# Acoustic - Escape the chaos with 'Relax and Unwind.' Tune in weekly for calming tracks that soothe your soul. Press play, breathe, and find peace in music.
-# Jazz - Sink into the laid-back vibes of 'Jazzy Lofi Lounge,' a weekly curated selection of smooth jazz beats. Perfect for unwinding and discovering new favorites.
